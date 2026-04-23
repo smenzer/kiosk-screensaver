a40672464496dae8cfc48b3351f0f4527513b99f
Scott Menzer <smenzer@gmail.com>
2026-04-23 09:12:04 -0400

    Add index page with page listing and repo link

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 scp screensaver.html root@kiosk-app.nj.menzer.org:/var/www/html/screensaver.html
+scp index.html root@kiosk-app.nj.menzer.org:/var/www/html/index.html
 echo "Deployed."
